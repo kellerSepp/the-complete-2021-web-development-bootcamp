cffd7645b8a381519031b696a4729a463ead7cab
kellerSeb <se.kellermann@gmail.com>
2021-07-18 13:35:19 +0200

    added human converter

--- projects/budgie-age-calculator/src/javascript/script.js
+++ projects/budgie-age-calculator/src/javascript/script.js
@@ -6,5 +6,5 @@ function calculateBudgieAge(){
 function calculateHumanAge(){
     var realAge = prompt("Please tell me your age.")
     var convertedAge = realAge * 8 / 72;
-    alert("Your budgie age is about " + convertedAge + ".");
+    alert("Your budgie age is about " + convertedAge.toString().slice(0,4) + " years.");
 }
