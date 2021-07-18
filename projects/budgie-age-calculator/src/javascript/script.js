b466cd2721db3d8cd90da3eac3a811d489e62a07
kellerSeb <se.kellermann@gmail.com>
2021-07-18 13:11:45 +0200

    initial budgie age calculator

---
+++ projects/budgie-age-calculator/src/javascript/script.js
@@ -0,0 +1,5 @@
+function calculateBudgieAge(){
+    var realAge = prompt("Please tell me the age of your budgie in actual years!")
+    var convertedAge = (realAge * 72) / 8;
+    alert(convertedAge)
+}
