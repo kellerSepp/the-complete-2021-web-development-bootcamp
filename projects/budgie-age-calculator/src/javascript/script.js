b086c0f9c77dc47b799203fceedd8072be941a04
kellerSeb <se.kellermann@gmail.com>
2021-07-18 13:16:16 +0200

    better outpoot

--- projects/budgie-age-calculator/src/javascript/script.js
+++ projects/budgie-age-calculator/src/javascript/script.js
@@ -1,5 +1,5 @@
 function calculateBudgieAge(){
     var realAge = prompt("Please tell me the age of your budgie in actual years!")
     var convertedAge = (realAge * 72) / 8;
-    alert(convertedAge)
+    alert("Your budgie / parakeet is about " + convertedAge + "years old in human years.");
 }
