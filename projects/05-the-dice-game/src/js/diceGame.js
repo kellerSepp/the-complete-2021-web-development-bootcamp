6398c160e1391e7c1439cb48495338b39846da87
kellerSeb <se.kellermann@gmail.com>
2021-07-20 11:06:15 +0200

    added dice functionality updating dice img depending on rolled dice

--- projects/05-the-dice-game/src/js/diceGame.js
+++ projects/05-the-dice-game/src/js/diceGame.js
@@ -1,5 +1,17 @@
+function gameLoop(){
+    changeDiceImage(1, rollDice(6));
+    changeDiceImage(2, rollDice(6));
+    
+}
+
 function rollDice(sides){
     let randomNumber1 = Math.ceil(Math.random()*6);
-
     console.log(randomNumber1);
+    return randomNumber1;
+}
+
+function changeDiceImage(player, number){
+    //dice1.png
+    document.getElementById("player" + player).lastElementChild.src="src/images/dice" + number + ".png";
+
 }
