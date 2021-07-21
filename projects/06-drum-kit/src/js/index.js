700d629dd0d7310336b671e9817cd24a7e4237e6
kellerSeb <https://github.com/kellerSepp>
2021-07-21 20:50:30 +0200

    play sound on button click
    added images

--- projects/06-drum-kit/src/js/index.js
+++ projects/06-drum-kit/src/js/index.js
@@ -1,1 +1,15 @@
-alert("GHello");
+function handleClick(){
+    let audio = new Audio("src/sounds/tom-1.mp3");
+    audio.play();
+}
+var elements = document.querySelectorAll("button");
+for(i=0; i<=elements.length-1;i++){
+
+    // elements[i].addEventListener("click",handleClick);
+}
+
+elements.forEach(function(currentValue, listObj) {
+    currentValue.addEventListener("click",handleClick);
+    console.log(currentValue.classList);
+  });
+
