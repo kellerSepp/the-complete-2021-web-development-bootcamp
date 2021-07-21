8e646f37c18c8634d65fc3fc7a20f40318198548
kellerSeb <https://github.com/kellerSepp>
2021-07-21 21:05:01 +0200

    buttons play different sounds

--- projects/06-drum-kit/src/js/index.js
+++ projects/06-drum-kit/src/js/index.js
@@ -1,6 +1,34 @@
 function handleClick(){
-    let audio = new Audio("src/sounds/tom-1.mp3");
-    audio.play();
+    switch(this.innerHTML){
+        case "w":  
+            var audio = new Audio("src/sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":  
+            var audio = new Audio("src/sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":  
+            var audio = new Audio("src/sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":  
+            var audio = new Audio("src/sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":  
+            var audio = new Audio("src/sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":  
+            var audio = new Audio("src/sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":  
+            var audio = new Audio("src/sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    }
 }
 var elements = document.querySelectorAll("button");
 for(i=0; i<=elements.length-1;i++){
