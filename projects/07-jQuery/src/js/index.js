fd72dbf8c79297fa937140eccfcd00c25d2a039f
kellerSeb <https://github.com/kellerSepp>
2021-07-25 19:59:58 +0200

    change h1 based on keypress

--- projects/07-jQuery/src/js/index.js
+++ projects/07-jQuery/src/js/index.js
@@ -7,4 +7,5 @@ $(document).keypress(function(event){
     console.info("%c" + event.key + "%cMr. %cGreen", "color: purple; text-decoration:underline;background-color:green;", "color:yellow");
     console.warn(event.key);
     console.error(event.key);
+    $("h1").html(event.key);
 });
