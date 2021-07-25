2a76f260079bb06b05d43624e70e3d0a97c531d2
kellerSeb <https://github.com/kellerSepp>
2021-07-25 19:40:33 +0200

    added click and keypress event tested other console. types

---
+++ projects/07-jQuery/src/js/index.js
@@ -0,0 +1,10 @@
+$("button").click(function(){
+    $("h1").css("color", "purple");
+});
+
+$(document).keypress(function(event){
+    console.log(event.key);
+    console.info("%c" + event.key + "%cMr. %cGreen", "color: purple; text-decoration:underline;background-color:green;", "color:yellow");
+    console.warn(event.key);
+    console.error(event.key);
+});
