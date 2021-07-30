14251f865b2f3d752b57821127f6315970d62620
kellerSeb <https://github.com/kellerSepp>
2021-07-30 15:11:32 +0200

    random number between 0-3 selectButton function added - select button based on number 0-3 number

--- projects/08-simon-game/src/js/game.js
+++ projects/08-simon-game/src/js/game.js
@@ -1,4 +1,36 @@
-function nextSequence(){
+function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     return randomNumber;
 }
+
+function selectButton(number) {
+    var audio;
+    switch (number) {
+        case 0:
+            $("#green").fadeOut(100).fadeIn(100);
+            audio = new Audio("/src/sounds/green.mp3");
+            audio.play();
+            break;
+        case 1: 
+            $("#red").fadeOut(100).fadeIn(100);
+            audio = new Audio("/src/sounds/red.mp3");
+            audio.play();
+            break;
+        case 2: 
+            $("#yellow").fadeOut(100).fadeIn(100);
+            audio = new Audio("/src/sounds/yellow.mp3");
+            audio.play();
+            break;
+        case 3:
+            $("#blue").fadeOut(100).fadeIn(100);
+            audio = new Audio("/src/sounds/blue.mp3");
+            audio.play();
+            break;
+        default: console.warn("Totally wrong random Number: " + number);
+            break;
+    }
+}
+
+// $("#someElement").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+// selectButton(nextSequence());
